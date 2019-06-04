3169908485ebc505c051fd9e41c4c2cd95629882
olemp <olemp@puzzlepart.com>
2019-06-04 12:27:46 +0200

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "./siteassets/js/bundle.js",
-        
-    },
-    mode:"development",
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            {
-              test: /\.tsx?$/,
-              use: [
-                {
-                  loader: 'ts-loader'                  
-                }
-              ]
-            },
-            {               
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{loader:"url-loader"}]                
-              },
-              {
-                test: /\.css$/,
-                use: [
-                  { loader: "style-loader" },
-                  { loader: "css-loader" }
-                ]
-              }
-          ]
-    },
-    externals: {
-   /*  "react":"React", 
-     "react-dom":"ReactDOM"*/
-    },
+  entry: "./src/index.tsx",
+  output: {
+    filename: "./siteassets/js/bundle.js",
+
+  },
+  mode: "development",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{ loader: "url-loader" }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
+  externals: {
+    /*  "react":"React", 
+      "react-dom":"ReactDOM"*/
+  },
 };
