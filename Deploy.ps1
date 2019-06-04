9717f0a09ea97e988450592973b692d05a57f4ef
olemp <olemp@puzzlepart.com>
2019-06-04 14:24:38 +0200

    Added files to root template

--- Deploy.ps1
+++ Deploy.ps1
@@ -5,9 +5,9 @@ $AppFilesPath = "C:\...";
 
 # Endre disse verdiene for å provisjonere på ditt miljø
 $env = @{   
-    SiteURL = "https://TENANT.sharepoint.com/"; 
-    User = "bruker@tenant.onmicrosoft.com"; 
-Pwd = ConvertTo-SecureString "Skriv inn passordet her" -AsPlainText -Force
+    SiteURL = "https://pzlcloud.sharepoint.com/sites/KomInn/"; 
+    User    = "olemp@puzzlepart.com"; 
+    Pwd     = ConvertTo-SecureString "Fgbk29gh2" -AsPlainText -Force
 }; 
 
 # Fjern kommentartegnet på dennne dersom du ikke allerede har installert SharePointPnPPowerShellOnline-modulen
@@ -17,12 +17,11 @@ $cred = New-Object -typename System.Management.Automation.PSCredential -Argument
 # Koble til SharePoint
 Connect-PnPOnline -Url $($env.SiteURL) -Credentials $cred
 
-Add-PnPFile -Path $($AppFilesPath + "dist\SiteAssets\js\bundle.js") -Folder "SiteAssets\js\"       
-Add-PnPFile -Path $($AppFilesPath + "dist\SiteAssets\css\Main.css") -Folder "SiteAssets\css\"       
-
-Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Home.aspx") -Folder "SitePages\"       
-Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\NyttForslag.aspx") -Folder "SitePages\"       
-Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Forslag.aspx") -Folder "SitePages\"       
-Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\SendTilKS.aspx") -Folder "SitePages\"  
-Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Vurdering.aspx") -Folder "SitePages\"            
+Add-PnPFile -Path $(".\dist\SiteAssets\js\bundle.js") -Folder "SiteAssets\js\"       
+Add-PnPFile -Path $(".\dist\SiteAssets\css\Main.css") -Folder "SiteAssets\css\"       
+# Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Home.aspx") -Folder "SitePages\"       
+# Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\NyttForslag.aspx") -Folder "SitePages\"       
+# Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Forslag.aspx") -Folder "SitePages\"       
+# Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\SendTilKS.aspx") -Folder "SitePages\"  
+# Add-PnPFile -Path $($AppFilesPath + "dist\SitePages\Vurdering.aspx") -Folder "SitePages\"            
    
